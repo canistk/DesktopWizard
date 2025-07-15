51ce77c766164393c285d5d00cd9b6593d5907de
canistk <canistk@gmail.com>
2025-07-16 03:06:06 +0800

    [Bug] werid bug that stopped async without any exception.
    // Loaded VRM model
    // ISSUE :
    // step 0 : load VRM,
    // step 1 : right click on VRM (GxWinPopup menu)
    // step 2 : load another VRM (fail, no exception detected)
    // not an issue _ Dig Vrm10.cs > Line 191, TryLoadingAsVrm10Async will pause without exception.
    // the awaitCaller will be hang for no reason.

--- Assets/Scripts/Loader/GxVRMLoader.cs
+++ Assets/Scripts/Loader/GxVRMLoader.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniGLTF;
 using UniVRM10;
+using System.Threading.Tasks;
 namespace Gaia
 {
     public static class GxVRMLoader
@@ -46,7 +47,101 @@ namespace Gaia
 				}
 
 				// Loaded VRM model
+				// ISSUE : 
+				// step 0 : load VRM,
+				// step 1 : right click on VRM (GxWinPopup menu)
+				// step 2 : load another VRM (fail, no exception detected)
+				// not an issue _ Dig Vrm10.cs > Line 191, TryLoadingAsVrm10Async will pause without exception.
+				// the awaitCaller will be hang for no reason.
+#if false
 				var vrm = await Vrm10.LoadPathAsync(path, true);
+#else
+				byte[] bytes = null;
+				using (FileStream fs = File.OpenRead(path))
+				{
+					using (BinaryReader binaryReader = new BinaryReader(fs))
+					{
+						bytes = binaryReader.ReadBytes((int)fs.Length);
+					}
+				}
+
+				const float TIMEOUT = 10f;
+
+				IAwaitCaller awaitCaller = Application.isPlaying
+					? new RuntimeOnlyAwaitCaller(TIMEOUT)
+					: new ImmediateCaller();
+
+				// attempt 1
+				// var _gltfData = new GlbLowLevelParser(string.Empty, bytes).Parse();
+
+				/*
+				// attempt 2
+				// remark : parser isn't the issue. AwaitCaller will not response correctly.
+				// will hang on next awaitCaller.
+				using var oper = awaitCaller.Run(() => new GlbLowLevelParser(string.Empty, bytes).Parse());
+				while (!oper.IsCompleted)
+				{
+					await Task.Yield();
+					//await awaitCaller.NextFrameIfTimedOut();
+				}
+				if (oper.IsCanceled)
+				{
+					Debug.LogError($"VRM loading canceled: {path}");
+					return;
+				}
+				if (oper.IsFaulted || oper.Exception != null)
+				{
+					if (fail != null)
+						fail.Invoke(oper.Exception);
+					return;
+				}
+				//*/
+
+				//*
+				// attempt 3
+				var _gltfData = await Task.Run(() =>
+				{
+					try
+					{
+						var rst = new GlbLowLevelParser(string.Empty, bytes).Parse();
+						return rst;
+					}
+					catch (System.Exception ex)
+					{
+						Debug.LogException(ex);
+						return null;
+					}
+				});
+				//*/
+
+				Vrm10Instance vrm = null;
+				try
+				{
+					using (var gltfData = _gltfData)
+					{
+						vrm = await Vrm10.LoadGltfDataAsync(gltfData,
+							canLoadVrm0X: true, // allow load vrm0x
+							controlRigGenerationOption: ControlRigGenerationOption.Generate,
+							showMeshes: true,
+							awaitCaller: awaitCaller,
+							textureDeserializer: null,
+							materialGenerator: null,
+							vrmMetaInformationCallback: null,
+							ct: default,
+							importerContextSettings: null
+						// springboneRuntime: null
+						);
+					}
+				}
+				catch (System.Exception ex)
+				{
+					throw ex;
+				}
+				finally
+				{
+					Debug.Log("should be Loaded");
+				}
+#endif
 
 				if (vrm == null)
 					throw new System.NullReferenceException($"Fail to load VRM at path {path}");
