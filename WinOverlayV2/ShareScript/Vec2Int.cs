c6a6fff349d300090f598a20d78ad77ddf5065ec
canistk <canistk@gmail.com>
2025-12-17 01:11:23 +0800

    remove non-used reference

--- WinOverlayV2/ShareScript/Vec2Int.cs
+++ WinOverlayV2/ShareScript/Vec2Int.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Runtime.InteropServices;
 #if UNITY_EDITOR || UNITY_STANDALONE
 using UnityEngine;
